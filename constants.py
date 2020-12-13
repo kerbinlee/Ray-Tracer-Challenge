270c3fea83406a74c7973812d6974149a3571115
Kevin Lee <lee.kevin@yahoo.com>
2020-12-12 17:06:04 -0800

    Chapter 3: Fix inverse matrix test case

--- constants.py
+++ constants.py
@@ -1,4 +1,3 @@
 class Constants:
     epsilon = 0.00001
-    epsilon_decimals = 4
     
--- tests/matrices_test.py
+++ tests/matrices_test.py
@@ -123,13 +123,13 @@ class TestMatrices(unittest.TestCase):
     def test_inverse_matrix(self):
         a = np.array([[-5, 2, 6, -8], [1, -5, 1, 8], [7, 7, -6, -7], [1, -3, 7, 4]])
         b = Matrix.inverse(a)
-        a_inverse = np.array([[0.21805, 0.45113, 0.24060, 0.04511], [-0.80827, -1.45677, -0.44361, 0.52068], [-0.07895, -0.22368, -0.05263, 0.19737], [-0.52256, -0.81391, -0.30075, 0.30639]])
+        a_inverse = np.array([[0.21805, 0.45113, 0.24060, -0.04511], [-0.80827, -1.45677, -0.44361, 0.52068], [-0.07895, -0.22368, -0.05263, 0.19737], [-0.52256, -0.81391, -0.30075, 0.30639]])
         self.assertAlmostEqual(Matrix.determinant(a), 532, delta = Constants.epsilon)
         self.assertAlmostEqual(Matrix.cofactor(a, 2, 3), -160, delta = Constants.epsilon)
         self.assertAlmostEqual(b[3][2], -160/532, delta = Constants.epsilon)
         self.assertAlmostEqual(Matrix.cofactor(a, 3, 2), 105, delta = Constants.epsilon)
         self.assertAlmostEqual(b[2][3], 105/532, delta = Constants.epsilon)
-        np.testing.assert_array_almost_equal(a_inverse, b, decimal = Constants.epsilon_decimals)
+        self.assertTrue(np.allclose(a_inverse, b, atol = Constants.epsilon))
 
     def test_inverse_matrix_1(self):
         a = np.array([[8, -5, 9, 2], [7, 5, 6, 1], [-6, 0, 9, 6], [-3, 0, -9, -4]])
@@ -149,4 +149,3 @@ class TestMatrices(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-    
